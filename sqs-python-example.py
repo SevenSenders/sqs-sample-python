87c1c0b7ae9fb3040503063448dffc7300bbdac9
Muhammad Sumon Molla Selim <sumonmselim@gmail.com>
2019-11-18 16:25:49 +0100

    updated endpoint url to live API url

--- sqs-python-example.py
+++ sqs-python-example.py
@@ -4,7 +4,7 @@ import boto3
 sqs = boto3.client(
     'sqs',
     region_name='eu-central-1',
-    endpoint_url='https://develop.analytics-api-dev.7senders.com/queue.xml',
+    endpoint_url='https://analytics-api.7senders.com/queue.xml',
     aws_access_key_id='your-7s-api-key-here', # 7S Shop API Key
     aws_secret_access_key='', # Keep it blank
 )
@@ -23,5 +23,4 @@ for message in messages:
     body = message.get('Body')
 
     print('{}: {}'.format(message_id, body))
-
-
+    
